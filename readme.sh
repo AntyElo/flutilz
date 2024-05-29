d768143bf398b57568143a0b96e453be311504aa
AntyElo <nougatoelo@yandex.com>
2024-05-29 19:49:38 +0300

    lcol3: ++unicode_chars

--- readme.sh
+++ readme.sh
@@ -1,22 +1,24 @@
-#!/bin/sh
-fg() { tput setaf $1; }
-bg() { tput setaf $1; }
-s()  { tput sgr0; }
+#!/bin/bash
+function [f() { tput setaf $1; }
+function [b() { tput setaf $1; }
+function c]()  { tput sgr0; }
 
 cat <<EOF
 Avaible utils:
-+ `fg 13 `lcol3` s` (and `fg 13 `lcol2` s`, `fg 13 `lcol1` s`): list\
- ` fg 1 `c` fg 2 `o` fg 3 `u` fg 4 `l` fg 5 `o` fg 6 `r` fg 7 `s` s`\
- and `fg 8 `c` fg 9 `o` fg 10 `u` fg 11 `l` fg 12 `o` fg 13 `r` fg 14 `s` s`,\
- `fg 8 `\$TERM` s` &c.
-+ `fg 13 `flu-hist` s`: `fg 11 `history` s`+`fg 11 `grep` s`
-+ `fg 13 `btpow*` s`: check power of your headphones
-+ `fg 13 `U` s` and `fg 13 `Ch` s`: S-^U whithout S-^U,\
- work in ttyN // `fg 11 `fbterm` s` // `fg 11 `tmux` s` e.g.:
-    `fg 13 `U` fg 7 ` 2023 2500 2502 2713 2717 2026 20 2014 20 41 72 65 20 67 6f 6f 64` s`
-    `fg 13 `Ch` fg 7 ` "‣─│✓✗… — Are good"` s`
-+ `fg 13 `cage+` s`: wrapper around cage
-+ `fg 13 `mpvt` s`: `fg 11 `mpv ` fg 7 `--vo=caca ` fg 8 `\$@` s`
++ `[f 13`lcol3`c]` (and `[f 13`lcol2`c]`, `[f 13`lcol1`c]`): list\
+ `[f 1`c`[f 2`o`[f 3`u`[f 4`l`[f 5`o`[f 6`r`[f 7`s`c]`\
+ and `[f 8`c`[f 9`o`[f 10`u`[f 11`l`[f 12`o`[f 13`r`[f 14`s`c]`,\
+ `[f 8`\$TERM`c]` &c.
+    * Set NOUTF if no love unicode promt
++ `[f 13`flu-hist`c]`: `[f 11`history`c]`+`[f 11`grep`c]`
++ `[f 13`btpow*`c]`: check power of your headphones
++ `[f 13`U`c]` and `[f 13`Ch`c]`: S-^U whithout S-^U,\
+ work in your favorit terminal e.g.:
+    `[f 13`U`c]` `[f 7`2023 U+2500 2502 2713 2717 2026 20 2014 20 41 72 65 20 67 6f 6f 64`c]`
+    `[f 13`Ch`c]` `[f 7`"‣─│✓✗… — Are good"`c]`
++ `[f 13`<util>+`c]`: wrapper around <util>, there are:
+    `[f 11`cage`c]`, `[f 11`fbgrab`c]`
++ `[f 13`mpvt`c]`: `[f 11`mpv`c]` `[f 7`--vo=caca`c]` `[f 8`\$@`c]`
 EOF
 
-unset bg fg s
+unset [f [b c]
